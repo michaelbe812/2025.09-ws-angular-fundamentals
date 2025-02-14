eb0cf3310a7c2d1b232f86d87227232fa17407ce
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-02-14 13:46:09 +0100

    chore: update to Angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
