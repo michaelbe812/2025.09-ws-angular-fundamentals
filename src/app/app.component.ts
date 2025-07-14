d915ac084f6b7d59ec908be4693d87d099e3d3c3
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-07-14 09:38:42 +0200

    feat(app): update welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'bookmonkey-client';
+  title = 'Angularian!';
 }
